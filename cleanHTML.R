130d16e4e49d24b44ee5fa364566c64778c70526
Harsh Anuj <harsh.anuj@gmail.com>
2018-10-31 12:45:51 -0400

    Add files via upload

---
+++ cleanHTML.R
@@ -0,0 +1,39 @@
+cleanHTML<-function(w){
+  
+  w<-lessons_text$Text[3]
+  
+  l<-gregexpr(pattern="<",text=w,fixed=T)[[1]]
+  
+  for(k in 1:length(l)){
+    
+    i<-gregexpr(pattern="<",text=w,fixed=T)[[1]]
+    
+    j<-gregexpr(pattern=">",text=w,fixed=T)[[1]]
+    
+    if(length(i)==1){
+      
+      a<-substr(x=w,start=i,stop=j)
+      
+      w<-gsub(pattern=a,replacement="",x=w,fixed=T)
+      
+      break
+      
+    }else{
+      
+      a<-substr(x=w,start=i[k],stop=j[k])
+      
+      w<-gsub(pattern=a,replacement="",x=w,fixed=T)
+      
+    }
+ 
+  }
+  
+  w<-gsub(pattern="[^a-zA-Z0-9\\.,:/ ]",replacement="",x=w)
+  
+  w<-trimws(w)
+  
+  w<-gsub(pattern="  ",replacement=" ",x=w)
+  
+  return(w)
+  
+}
